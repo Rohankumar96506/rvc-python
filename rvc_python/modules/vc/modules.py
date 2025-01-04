b74c63a4a05aaa05d4d2957f8c3a01277d78edaf
Rohankumar96506 <59435105+Rohankumar96506@users.noreply.github.com>
2025-01-04 18:15:17 +0530

    Update modules.py

--- rvc_python/modules/vc/modules.py
+++ rvc_python/modules/vc/modules.py
@@ -103,6 +103,7 @@ class VC:
         self.cpt = torch.load(sid, map_location="cpu")
         print(f"Config: {self.cpt['config']}")
         print(f"Is Half: {self.config.is_half}")
+        print(f"type: {type(self.cpt)}")
         
         self.tgt_sr = self.cpt["config"][-1]
         self.cpt["config"][-3] = self.cpt["weight"]["emb_g.weight"].shape[0]  # n_spk
